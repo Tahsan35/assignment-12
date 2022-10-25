a15ca4eeecb47008e9266030162956959a68e614
Tahsan <tahsanknmas14@gmail.com>
2022-10-25 14:39:30 +0600

    complete admin hook

---
+++ src/Pages/Hooks/UseAdmin.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+const UseAdmin = (user) => {
+    const [admin, setAdmin] = useState(false);
+    const [adminLoading, setAdminLoading] = useState(true);
+
+    useEffect(() => {
+        const email = user?.email;
+        if (email) {
+            fetch(`https://sleepy-hollows-57490.herokuapp.com/admin/${email}`, {
+                method: 'GET',
+                headers: {
+                    'content-type': 'application/json',
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setAdmin(data.admin);
+                    setAdminLoading(false);
+                })
+        }
+    }, [user]);
+    return [admin, adminLoading]
+};
+
+export default UseAdmin;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import 'antd/dist/antd.css';
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
