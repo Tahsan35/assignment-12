2117eb94f3afe33084c4f907d6e0324fe03bec58
Tahsan <tahsanknmas14@gmail.com>
2022-10-25 21:52:42 +0600

    complete the components

---
+++ src/Pages/Purchase/Purchase.js
@@ -0,0 +1,55 @@
+import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Loader from '../Shared/Loader';
+import PurchaseModal from './PurchaseModal';
+const Purchase = () => {
+    const { id } = useParams()
+    const [purchase, setPurchase] = useState(null);
+
+
+    const { data: product, isLoading, refetch } = useQuery('parts', () =>
+        fetch(`https://sleepy-hollows-57490.herokuapp.com/parts/${id}`, {
+            method: 'GET',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        }).then(res => res.json())
+
+    )
+    if (isLoading) {
+        return <Loader></Loader>
+    }
+    const { name, img, price, info, orderQuantity, quantity, _id } = product;
+
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <div className="card w-72 md:w-96 bg-base-100 shadow-xl image-full mx-auto">
+                <figure><img src={img} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <div>
+                        <p className='mb-2'>{info}</p>
+                        <p className='mb-1'>Order Quantity: {orderQuantity}</p>
+                        <p className='mb-1'>Available Quantity: {quantity}</p>
+                        <p>Price per Unit: <span className='font-bold'>${price}</span></p>
+                    </div>
+                    <div className="card-actions justify-end">
+                        <label
+                            onClick={() => setPurchase(product)}
+                            htmlFor="purchaseModal"
+                            className='btn bg-cyan-500 hover:bg-cyan-600 cursor-pointer text-center uppercase'
+                        >purchase</label>
+                    </div>
+                </div>
+            </div>
+            {purchase && <PurchaseModal
+                purchase={purchase}
+                setPurchase={setPurchase}
+                refetch={refetch}
+            ></PurchaseModal>}
+        </div>
+    );
+};
+
+export default Purchase;
---
+++ src/Pages/Purchase/PurchaseModal.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
+
+const PurchaseModal = ({ purchase, setPurchase, refetch }) => {
+    const { name, orderQuantity, quantity, _id, price } = purchase;
+    const [user] = useAuthState(auth);
+
+    // function for update quantity after buyer purchase
+    const handleQuantity = ([quantity, orderAmount]) => {
+        const newQuantity = parseInt(quantity) - parseInt(orderAmount);
+
+        fetch(`https://sleepy-hollows-57490.herokuapp.com/parts/${_id}`, {
+            method: "PUT",
+            body: JSON.stringify({ newQuantity: newQuantity }),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                refetch();
+            });
+    }
+
+    // handle purchase function
+    const handleSubmit = event => {
+        event.preventDefault();
+        const orderAmount = event.target.quantity.value
+        const totalPrice = price;
+        if (orderAmount < orderQuantity) {
+            return alert('please Enter Order Quantity 500 or Longer');
+        }
+
+        if (quantity < orderAmount) {
+            return toast('You order more then Available Quantity');
+        }
+        handleQuantity([quantity, orderAmount]);
+
+        const purchase = {
+            purchaseProduct: event.target.productName.value,
+            buyerName: event.target.name.value,
+            buyerEmail: event.target.email.value,
+            orderAmount,
+            totalPrice,
+            phone: event.target.phone.value,
+        }
+        fetch('https://sleepy-hollows-57490.herokuapp.com/purchase', {
+            method: "POST",
+            body: JSON.stringify(purchase),
+            headers: {
+                'Content-type': 'application/json'
+            },
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    toast.success('Purchase Item Successfully');
+                    setPurchase(null);
+                    refetch();
+                } else {
+                    toast.error('Try Again')
+                }
+            })
+    };
+    return (
+        <div>
+            <input type="checkbox" id="purchaseModal" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box relative">
+                    <label htmlFor="purchaseModal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 className="text-lg font-bold">Purchase for {name}</h3>
+                    <form onSubmit={handleSubmit} className='grid grid-cols-1 gap-3 justify-items-center mt-2'>
+                        <input type="text" name="productName" disabled value={name} className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name="name" disabled value={user?.displayName || ''} className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name="email" disabled value={user?.email || ''} className="input input-bordered w-full max-w-xs" />
+                        <input required type="number" name="quantity" min={orderQuantity} placeholder={`Minimum Order Quantity ${orderQuantity}`} className="input input-bordered w-full max-w-xs" />
+                        <input required type="number" name="phone" placeholder='Phone Number' className="input input-bordered w-full max-w-xs" />
+                        <input
+                            disabled={quantity === 0}
+                            type="submit"
+                            value="Purchase"
+                            className="input cursor-pointer input-bordered w-full max-w-xs bg-[#3A4256] text-white" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PurchaseModal;
